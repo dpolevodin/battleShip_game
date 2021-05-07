939391736ecb621aa460f3a6a710648c762301cb
dpolevodin <dpolevodin@gmail.com>
2021-05-07 12:55:55 +0500

    update 07.05.2021, alpha version

--- battleship.js
+++ battleship.js
@@ -13,14 +13,105 @@ let view = {
     }
 };
 
-let ships = [{ location: ['10', '20', '30'], hits: ['', '', ''] },
-             { location: ['32', '33', '34'], hits: ['', '', ''] },
-             { location: ['63', '64', '65'], hits: ['', '', 'hit'] }];
+let model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLength: 3,
+    shipSunk: 0,
 
-//view.displayMiss('00');
-//view.displayHit('34');
-//view.displayMiss('55');
-//view.displayHit('12');
-//view.displayMiss('25');
-//view.displayHit('26');
-//view.displayMessage('Testing message')
+    ships: [{ locations: ['06', '16', '26'], hits: ['', '', ''] },
+            { locations: ['24', '34', '44'], hits: ['', '', ''] },
+            { locations: ['10', '11', '12'], hits: ['', '', ''] }],
+    
+    fire: function(guess) {
+        for (let i = 0; i < this.numShips; i++) {
+            let ship = this.ships[i];
+            let index = ship.locations.indexOf(guess);
+            if (index >= 0) {
+                ship.hits[index] = 'hit';
+                view.displayHit(guess);
+                view.displayMessage('HIT!');
+                if (this.isSunk(ship)) {
+                    view.displayMessage('You sunk my battleship!');
+                    this.shipSunk++;
+                }
+                return true;
+
+            }
+        }
+        view.displayMiss(guess);
+        view.displayMessage('You missed.');
+        return false;
+    },
+
+    isSunk: function(ship) {
+        for (let i = 0; i < this.shipLength; i++) {
+            if (ship.hits[i] !== 'hit') {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+let controller = {
+    guesses: 0,
+
+    processGuess: function(guess) {
+        var location = parseGuess(guess);
+        if (location) {
+            this.guesses++;
+            let hit = model.fire(location);
+            if (hit && model.shipSunk == model.numShips) {
+                view.displayMessage('You sunk all my battleships, in  ' + this.guesses + ' guesses');
+            }
+        }
+    }
+};
+
+function parseGuess(guess) {
+    const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+
+
+    if (guess === null || guess.length !== 2) {
+        alert('Oops, please enter a letter and a number on the board');
+    } else {
+        firstChar = guess.charAt(0);
+        let row = alphabet.indexOf(firstChar);
+        let column = guess.charAt(1);
+
+        if (isNaN(row) || isNaN(column)) {
+            alert("Oops, that isn't on the board");
+        } else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) {
+            alert("Oops, that isn't on the board");
+        } else {
+            return row + column;
+        }
+    }
+    return null;
+};
+
+function init() {
+    let fireButton = document.getElementById('fireButton');
+    fireButton.onclick = handleFireButton;
+    let guessInput = document.getElementById('guessInput');
+    guessInput.onkeypress = handleKeyPress;
+};
+
+function handleFireButton() {
+    let guessInput = document.getElementById('guessInput');
+    let guess = guessInput.value;
+    controller.processGuess(guess);
+
+    guessInput.value = '';
+};
+
+function handleKeyPress(e) {
+    let fireButton = document.getElementById('fireButton');
+    if (e.keyCode === 13) {
+        fireButton.click();
+        return false;
+    }
+}
+
+window.onload = init;
